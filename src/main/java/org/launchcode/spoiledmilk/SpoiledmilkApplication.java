3346a44e379f5b614fb46ea39c804011ecdf98ce
Richard WIlliams <riwi2802@colorado.edu>
2022-01-04 16:19:28 -0500

    first commit, created barest of bare bones controller and homepage

---
+++ src/main/java/org/launchcode/spoiledmilk/SpoiledmilkApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.spoiledmilk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpoiledmilkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpoiledmilkApplication.class, args);
+	}
+
+}
---
+++ src/main/java/org/launchcode/spoiledmilk/controllers/HomeController.java
@@ -0,0 +1,20 @@
+package org.launchcode.spoiledmilk.controllers;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+
+    @RequestMapping("")
+    public String displayHomePage(Model model) {
+
+
+        return "Home";
+
+    }
+}
