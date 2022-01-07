688149c1a0bc9ce37433da3db99c1a80b16b2431
Richard WIlliams <riwi2802@colorado.edu>
2022-01-06 19:13:03 -0500

    added first draft of data models

---
+++ src/main/java/org/launchcode/spoiledmilk/models/PurchaseEntry.java
@@ -0,0 +1,21 @@
+package org.launchcode.spoiledmilk.models;
+
+public class PurchaseEntry {
+
+
+    //data about purchase
+    private String date;
+    private String time;
+
+
+    private boolean lookedLikeThereWasALotLeft;
+    private boolean wasLastGallon;
+
+    //data about milk
+    private String milkBrand;
+    private String milkSize;
+    private String milkType;
+    private boolean isOrganic;
+
+    private String price;
+}
---
+++ src/main/java/org/launchcode/spoiledmilk/models/Store.java
@@ -0,0 +1,8 @@
+package org.launchcode.spoiledmilk.models;
+
+public class Store {
+
+    private String name;
+
+    private int streetNum;
+}
